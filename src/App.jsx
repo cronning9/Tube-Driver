cac5f05e9d966f469912d0c5198931e18691106b
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-27 22:15:39 -0500

    rf

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React from 'react';
 
 import Header from './Header/Header';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-      <Header/>
-        <p className="app-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () =>
+  <div className="app">
+    <Header/>
+    <p className="app-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+
 
 export default App;
